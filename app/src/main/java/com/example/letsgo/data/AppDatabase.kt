828c792b80fd726ea195810fd69a9835bf317e68
Long Nguyen <18nguyenl@gmail.com>
2020-06-16 00:51:21 -0500

    Polish some minor styling errors

--- app/src/main/java/com/example/letsgo/data/AppDatabase.kt
+++ app/src/main/java/com/example/letsgo/data/AppDatabase.kt
@@ -4,12 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.letsgo.models.Hashtag
 import com.example.letsgo.models.Task
 
 const val DATABASE_NAME = "lfg-db"
 
-@Database(entities = arrayOf(Task::class, Hashtag::class), version = 1, exportSchema = false)
+@Database(entities = arrayOf(Task::class), version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
 
