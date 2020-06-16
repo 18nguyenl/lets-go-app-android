90cf604c09f7c75e7053b94fb7ba3d6d8f9eb30c
ambitiousmustard <cbirchallroman@gmail.com>
2020-06-16 01:41:09 -0500

    Added taskIDs to Hashtag model, added hashtagIDs to Task model, added fetch functions by name or by ID to Hashtag VM

--- app/src/main/java/com/example/letsgo/models/Hashtag.kt
+++ app/src/main/java/com/example/letsgo/models/Hashtag.kt
@@ -8,7 +8,8 @@ import androidx.room.PrimaryKey
 data class Hashtag(
     @ColumnInfo(name = "name") val name: String,    // a hashtag's name contains its parent directories, eg. "Parent1/Parent2/ThisTag"
     @ColumnInfo(name = "parentID") val parentID: Int,
-    @ColumnInfo(name = "childIDs") val childIDs: HashSet<Int>
+    @ColumnInfo(name = "childIDs") val childIDs: HashSet<Int>,
+    @ColumnInfo(name = "taskIDs") val taskIDs: HashSet<Int>
 ) {
 
     @PrimaryKey(autoGenerate = true) var id: Int = 0
@@ -19,6 +20,18 @@ data class Hashtag(
 
     fun addChild(childID: Int){
         childIDs.add(childID)
+    }
+
+    fun removeChild(childID: Int){
+        childIDs.remove(childID)
+    }
+
+    fun addTask(taskID: Int){
+        taskIDs.add(taskID)
+    }
+
+    fun removeTask(taskID: Int){
+        taskIDs.remove(taskID)
     }
 
     companion object{
--- app/src/main/java/com/example/letsgo/viewmodels/HashtagViewModel.kt
+++ app/src/main/java/com/example/letsgo/viewmodels/HashtagViewModel.kt
@@ -13,41 +13,51 @@ import kotlinx.coroutines.runBlocking
 
 class HashtagViewModel(private val dao: DataAccessObject<Hashtag>) : ViewModel() {
 
-    fun createHashtag(hashtag: String) : Hashtag{
+
+
+    fun createHashtag(hashtag: String) : Hashtag {
 
         // if hashtag is invalid, throw an error
         if(!HashtagUtils.verifyHashtag(hashtag))
             throw Error()
 
-        // we will not expect to create any child
-        val childIDs: HashSet<Int> = HashSet()
+        // set parent ID to non-existent for now
         var parentID: Int = 0
 
         //if this hashtag is a child of another, check if it exists
         val parentName = HashtagUtils.getPath(hashtag)
         if(!parentName.equals("")){
 
-            val parentTag: Hashtag? = dao.fetchByQuery(SimpleSQLiteQuery("SELECT * FROM ${Hashtag.hashtagTable} WHERE name = $parentName")).value
+            val parentTag: Hashtag? = fetchHashtagByName(parentName)
 
             // set parentID to this tag if it exists, otherwise create it
             parentID = parentTag?.id ?: createHashtag(parentName).id
 
         }
 
-        // create hashtag and insert into database
-        val creation = Hashtag(hashtag, parentID, childIDs)
-        dao.insert(Hashtag(hashtag, parentID, childIDs))
+        // create hashtag and insert into database (sets of child IDs and task IDs will be empty)
+        val creation = Hashtag(hashtag, parentID, HashSet(), HashSet())
+        dao.insert(creation)
 
         // if this tag has a parent, add this tag as a child and update the parent (WHICH MUST NECESSARILY EXIST NOW)
         if(parentID != 0){
 
-            val parentTag: Hashtag = dao.fetchByQuery(SimpleSQLiteQuery("SELECT * FROM ${Hashtag.hashtagTable} WHERE name = $parentName")).value!!
+            val parentTag: Hashtag = fetchHashtagByID(parentID)!!
             parentTag.addChild(creation.id)
             dao.update(parentTag)
 
         }
 
         return creation
+
+    }
+
+    fun fetchHashtagByID(id: Int) : Hashtag? = dao.fetchByID(id).value
+    fun fetchHashtagByName(name: String) : Hashtag? = dao.fetchByQuery(SimpleSQLiteQuery("SELECT * FROM ${Hashtag.hashtagTable} WHERE name = $name")).value
+
+    fun tagTask(hashtagID: Int, taskID: Int) {
+
+    
 
     }
 
